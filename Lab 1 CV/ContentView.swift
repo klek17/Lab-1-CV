4951dedf9d8b64c9814757949de24c0f6f979b1c
klek17 <kevin.limek@gmail.com>
2026-01-21 11:00:38 +0100

    2nd commit

--- Lab 1 CV/ContentView.swift
+++ Lab 1 CV/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 import SwiftData
 
 struct ProfileHeaderView: View {
+    let imageName: String
     let name: String
     let role: String
-    
+    let city: String
+        
     var body: some View {
         VStack(alignment: .center, spacing: 0){
-            Image(.asdf).resizable().aspectRatio(contentMode: .fill).frame(width: 100, height: 100).clipShape(Circle())
+            Image(imageName).resizable().aspectRatio(contentMode: .fill).frame(width: 100, height: 100).clipShape(Circle())
             Text(name).font(.largeTitle).bold()
             Text(role).font(.title2)
+            Text(city).font(.title3)
         }
     }
 }
@@ -40,10 +43,48 @@ struct ContentView: View {
 
     var body: some View {
         VStack(spacing: 16){
+            
             ProfileHeaderView(
+                imageName: "asdf",
                 name: "Kevin Lim",
-                role: "iOS Student"
+                role: "iOS Student",
+                city: "City: Singapore"
             )
+            
+            VStack(spacing: 12){
+                NavigationSplitView {
+                    Text("List of previous workplace/school")
+                    List {
+                        NavigationLink {
+                            Text("North View Primary School")
+                        } label: {
+                            Text("Primary School")
+                        }
+                        NavigationLink {
+                            Text("Presbyterian High School")
+                        } label: {
+                            Text("Secondary School")
+                        }
+                        NavigationLink {
+                            Text("North View Primary School")
+                        } label: {
+                            Text("High Sch")
+                        }
+                        
+                        
+                        ForEach(items) { item in
+                            NavigationLink {
+                                Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                            } label: {
+                                Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                    }
+                } detail: {
+                    Text("Select an item")
+                }
+            }
             
             HStack(spacing: 24){
                 ActionItemView(systemImage: "message.fill", label: "Message")
